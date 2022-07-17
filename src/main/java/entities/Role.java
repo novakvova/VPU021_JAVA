6c2b1af67e6834dc41eafcc089a86c19b12b9fbe
novakvova <novakvova@gmail.com>
2022-07-17 11:52:58 +0300

    Begin hibernate

---
+++ src/main/java/entities/Role.java
@@ -0,0 +1,17 @@
+package entities;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="tbl_roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 250, nullable = false)
+    private String name;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,22 @@
 package org.example;
 
+import entities.Role;
+
+import java.io.PrintStream;
 import java.util.Scanner;
+import org.hibernate.Session;
+import utils.HibernateSessionFactoryUtil;
+
 
 public class Main {
-    public static void main(String[] args) {
-        //long, int, byte, float, double, boolean, char,
-        //String str;
-        int a;
-        System.out.println("Вкажіть значення");
-        Scanner in = new Scanner(System.in);
-        a =  in.nextInt();
-        if(a==12)
-            System.out.println("Найкраща оцінка");
-        else
-            System.out.println("Треба підтягуватися");
+    public static void main(String[] args) throws Exception {
+        System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        Scanner in = new Scanner(System.in, "UTF-8");
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        session.close();
+        Role role=new Role();
+        role.setName("Admin");
+        System.out.println("Role "+role.getName());
+
     }
 }
